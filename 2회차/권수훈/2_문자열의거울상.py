016a8ebd79ef3717c938ba5c33cb9f547025381f
gownsuhun <rhqemd333@naver.com>
2022-08-01 14:31:09 +0900

    실습

--- 2회차/권수훈/2_문자열의거울상.py
+++ 2회차/권수훈/2_문자열의거울상.py
@@ -1,3 +1,22 @@
 import sys
 
-sys.stdin = open("_문자열의거울상.txt")
+# ‘b’, ‘d’, ‘p’, ‘q’로 이루어진 문자열이 주어진다. 이 문자열을 거울에 비추면 어떤 문자열이 되는지 구하는 프로그램을 작성하라.
+
+# 예를 들어, “bdppq”를 거울에 비추면 “pqqbd”처럼 나타날 것이다.
+
+
+
+#count_num = int(input())
+
+
+upsideDown = {
+    "b" : "d",
+    "d" : "b",
+    "p" : "q",
+    "q" : "p"   
+}
+for k,y in upsideDown:
+    #키의값이 입력되면 밸류의값인 d 가나오게 하고싶었다. 
+    #어제 다이얼문제랑 비슷한거같은데... 구글링을 못하니까 힘들다
+
+ sys.stdin = open("_문자열의거울상.txt")
--- 2회차/권수훈/3_직사각형길이찾기.py
+++ 2회차/권수훈/3_직사각형길이찾기.py
@@ -1,3 +1,15 @@
 import sys
 
+
+number = int(input())
+for i in range(1,number+1):
+    A,B,C  = map(int,(input().split()))
+    if A == B:
+        print(f"#{i} {C}")
+    elif A == C:
+        print(f"#{i} {B}")
+    elif B == C:
+        print(f"#{i} {A}")
+
+# 직사각형의 3개의 길이를 알려줘서  3개중 다른 1개의 길이가 구하고 싶은 길이랑 같다.
 sys.stdin = open("_직사각형길이찾기.txt")
